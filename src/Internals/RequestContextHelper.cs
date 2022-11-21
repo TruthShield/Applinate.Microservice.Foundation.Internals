580dd56c09becd049a902f9522028309b8519718
Matthew Cochran <matthew@cochranweb.com>
2022-11-21 14:38:57 -0500

    Init

---
+++ src/Internals/RequestContextHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Applinate.Internals
+{
+    public static class RequestContextHelper
+    {
+        public static void SetCurrentRequestContext(this RequestContext ctx) =>
+            RequestContext.Current = ctx;
+
+        public static RequestContext GetCurrentRequestContext() => 
+            RequestContext.Current;
+
+        internal static void SetCurrentRequestContextAsClient() =>
+            RequestContext.Current = RequestContext.Current with 
+            { 
+                ServiceType = ServiceType.Client 
+            };
+    }
+}
